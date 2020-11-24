e5a5a320402e99ca81eea15313389159f332e534
l00448061 <liufanfan900618@163.com>
2020-11-24 10:14:16 +0800

    1.V2.0,信息增加网元类型、网络类型、地区、省份信息

--- Illegal_segment_Analysis.py
+++ Illegal_segment_Analysis.py
@@ -6,6 +6,42 @@ import xlwt
 logging.basicConfig(filename='mylog.txt', format="%(asctime)s : %(message)s",
                     level=logging.DEBUG)
 
+# 网元映射关系(十六进;制)
+NFTypeDict = {'00': 'reserved', '01': '5G-EIR', '02': 'AF', '03': 'AMF', '04': 'AUSF', '05': 'BSF',
+              '06': 'CHF', '07': 'GMLC', '08': 'LMF', '09': 'N3IWF', '0A': 'NEF', '0B': 'NRF',
+              '0C': 'NSSF', '0D': 'NWDAF', '0E': 'PCF', '0F': 'SEPP', '10': 'SMF', '11': 'SMSF',
+              '12': 'UDM', '13': 'UDR', '14': 'UDSF', '15': 'UPF'}
+
+# 大区映射关系
+regionDict = {'01': '中部大区', '02': '西北大区', '03': '南部大区', '04': '西南大区',
+              '05': '东部大区', '06': '北部大区', '07': '上海大区', '08': '北京大区'}
+
+# 省份映射关系
+provinceDict = {'80': '广东', '40': '广西', '20': '海南', '10': '湖南', '08': '福建'}
+
+# 网络类型
+NetwokTypeDict = {'00': '人网', '01': '物网'}
+
+'''
+人网AMF查询的SMF NF cache信息，不能超过如下范围
+广东:     750000~7AFFFF       810000~86FFFF
+
+物网AMF查询的SMF NF cache信息，不能超过如下范围：
+广东:     750000~7AFFFF       810000~86FFFF
+湖南:     870000~89FFFF       8D0000~8DFFFF
+广西:     B30000~B5FFFF       BA0000~BAFFFF
+福建:     5E0000~60FFFF       680000~68FFFF
+海南:     C00000~C0FFFF       C20000~C2FFFF
+'''
+NFCacheRange2C = {'750000': '7AFFFF', '810000': '86FFFF'}
+
+# 物网AMF查询的SMF NF cache信息，不能超过如下范围
+NFCacheRange2B = {'750000': '7AFFFF', '810000': '86FFFF',
+                  '870000': '89FFFF', '8D0000': '8DFFFF',
+                  'B30000': 'B5FFFF', 'BA0000': 'BAFFFF',
+                  '5E0000': '60FFFF', '680000': '68FFFF',
+                  'C00000': 'C0FFFF', 'C20000': 'C2FFFF', }
+
 
 # 读取文件夹下txt
 def TXTFileList():
@@ -33,6 +69,20 @@ def illegalRule(d1, d2):
             res = d1.strip() + ' ' + d2.strip()
     if len(d1.split(':')[1].rstrip(',').strip()) != len(d2.split(':')[1].strip()):
         res = d1.strip() + ' ' + d2.strip()
+
+    # SMF TAC号段： {"start": "750000","end": "7affff"}
+    if len(d1.split(':')[1].rstrip(',').strip()) == 6 and len(d2.split(':')[1].strip()) == 6:
+        iStart = int(d1.split(':')[1].rstrip(',').strip(), 16)
+        iEnd = int(d2.split(':')[1].rstrip(',').strip(), 16)
+        flag = 0
+        for key in NFCacheRange2B:
+            iStartRange = int(key.upper(), 16)
+            iEndRange = int(NFCacheRange2B[key].upper(), 16)
+            if iStart >= iStartRange and iEnd <= iEndRange:
+                flag = 1
+                break
+        if flag == 0:
+            res = d1.strip() + ' ' + d2.strip()
     return res
 
 
@@ -48,25 +98,11 @@ def txtAnalysis(filePath):
     # 将数据按照‘DSP NFCACHE: QUERYTYPE=NFID’分割，相同的放到一个list中
     NFID = ''
     for dts in dataLists:
-        if 'DSP NFCACHE: QUERYTYPE=NFID' in dts:
-            dflag = 0
-            if NFID != dts.split('NFID=')[1]:
-                NFID = dts.split('NFID=')[1]
-                dataList.append(dataListTmp)
-                dataListTmp = []
+        if 'nfInstanceId:' in dts:
+            dataList.append(dataListTmp)
+            dataListTmp = []
         dataListTmp.append(dts)
     dataList.append(dataListTmp)
-    # for i in dataList:
-    #     if 'fqdn' not in i:
-    #         print(i)
-
-    #
-    # # 将数据按照‘个结果’分割，放入list数组
-    # for dts in dataLists:
-    #     dataListTmp.append(dts)
-    #     if '个结果' in dts:
-    #         dataList.append(dataListTmp)
-    #         dataListTmp = []
 
     # 循环遍历各数组
     illegaldic = {}
@@ -74,12 +110,17 @@ def txtAnalysis(filePath):
         flag = 0
         illegalList = []
         segNum = 0
-        fqdn = ''
+        if 'nfInstanceId' in dList[0]:
+            nfInstanceId = str(dList[0].split(':')[1].rstrip(',').strip().split('-')[-1][0:6])
 
+        else:
+            continue
+        if 'nfType' in dList[1]:
+            nfType = dList[1].split(':')[1].rstrip(',').strip()
         for dt in range(1, len(dList)):
-            # 计算号段个数
-            if 'start' in dList[dt]:
-                segNum += 1
+            # # 计算号段个数
+            # if 'start' in dList[dt]:
+            #     segNum += 1
             dt1 = dList[dt - 1]
             dt2 = dList[dt]
             if (dt + 1) < len(dList):
@@ -98,23 +139,26 @@ def txtAnalysis(filePath):
             if 'start' in dt1 and 'end' not in dt2 and 'end' in dt4:
                 if len(illegalRule(dt1, dt4)):
                     illegalList.append(illegalRule(dt1, dt4))
-            if 'fqdn' in dList[dt] and flag == 0:
-                fqdn = dList[dt]
+            if 'MENAME:' in dList[dt] and flag == 0:
+                # UNC / * MEID: 0   MENAME: GD_GD_GZ_AMF800_C_HW */  解析出网络类型；_C_
+                netwokType = dList[dt].split('MENAME:')[1].split('*')[0].split('_')[-2]
                 flag = 1
 
         keyflag = 0
-        # key： [fqdn:号段个数]     value： [非法号段]
+        # key： [nfInstanceId=nfType]     value： [非法号段]
         for dickey in illegaldic:
-            if fqdn in dickey:
-                key = dickey.split('=')[0] + '=' + str(int(dickey.split('=')[1]) + segNum)
-                illegaldic[key] = illegaldic.pop(dickey)
+            if nfInstanceId in dickey:
+                # key = dickey.split('=')[0] + '=' + str(int(dickey.split('=')[1]) + segNum)
+                # illegaldic[key] = illegaldic.pop(dickey)
                 illegaldic[key].extend(illegalList)
                 keyflag = 1
                 break
         if keyflag == 0:
-            key = fqdn + '=' + str(segNum)
+            key = str(nfInstanceId) + '=' + str(nfType) + '=' + str(netwokType)
             illegaldic[key] = illegalList
     file.close()
+    for key in illegaldic:
+        print(key, ':', illegaldic[key])
     return illegaldic
 
 
@@ -159,6 +203,47 @@ def SetFont(type):
     return style
 
 
+def MatchData(nfID):
+    NFType = str(nfID[0:2])
+    MDataDict = {}
+    region = str(nfID[2:4])
+    province = str(nfID[4:6])
+    NetwokType = str(nfID[6:8])
+    flag = 0
+    for NFTyeKey in NFTypeDict:
+        if NFType == NFTyeKey:
+            MDataDict['NFType'] = NFTypeDict[NFTyeKey]
+            flag = 1
+            break
+    if flag == 0:
+        MDataDict['NFType'] = ''
+    flag = 0
+    for regionKey in regionDict:
+        if region == regionKey:
+            MDataDict['region'] = regionDict[regionKey]
+            flag = 1
+            break
+    if flag == 0:
+        MDataDict['region'] = ''
+    flag = 0
+    for provinceKey in provinceDict:
+        if province == provinceKey:
+            MDataDict['province'] = provinceDict[provinceKey]
+            flag = 1
+            break
+    if flag == 0:
+        MDataDict['province'] = ''
+    flag = 0
+    for NetwokTypeKey in NetwokType:
+        if NetwokType == NetwokTypeKey:
+            MDataDict['NetwokType'] = NetwokType[NetwokTypeKey]
+            flag = 1
+            break
+    if flag == 0:
+        MDataDict['NetwokType'] = ''
+    return MDataDict
+
+
 def XLSWrite(XLSPath, illegalData):
     # 写数据时，行计数器
     logging.info('xls write begin')
@@ -171,9 +256,11 @@ def XLSWrite(XLSPath, illegalData):
     bodyFont1 = SetFont(3)  # 垂直居中
     bodyFont2 = SetFont(4)  # 水平垂直居中
 
-    sht1.write(0, 0, 'fqdn', headFont)
-    sht1.write(0, 1, '号段总数', headFont)
-    sht1.write(0, 2, '非法号段', headFont)
+    sht1.write(0, 0, '网元类型', headFont)
+    sht1.write(0, 1, '大区', headFont)
+    sht1.write(0, 2, '省份', headFont)
+    sht1.write(0, 3, '网络类型', headFont)
+    sht1.write(0, 4, '冲突号段', headFont)
 
     shtNum1 = 1
 
@@ -181,23 +268,32 @@ def XLSWrite(XLSPath, illegalData):
     # sheet1
     rowBegin = 1
     for illData in illegalData:
-        if 'fqdn' in illData:
-            fqdn = illData.split('=')[0].rstrip(',').split(':')[1].strip(' ')
-            segNum = illData.split('=')[1]
+        MdataDict = MatchData(illData.split('=')[0])
+        NFType = illData.split('=')[1]
+        networkType = illData.split('=')[2]
+        nType = ''
+        if networkType == 'C':
+            nType = '人网'
+        if networkType == 'B':
+            nType = '物网'
 
-            if len(illegalData[illData]):
-                sht1.write_merge(rowBegin, rowBegin + len(illegalData[illData]) - 1, 0, 0, fqdn, bodyFont1)
-                sht1.write_merge(rowBegin, rowBegin + len(illegalData[illData]) - 1, 1, 1, segNum, bodyFont2)
-                shtNum1 = rowBegin
-                for ld in illegalData[illData]:
-                    sht1.write(shtNum1, 2, ld, bodyFont2)
-                    shtNum1 = shtNum1 + 1
-                rowBegin += len(illegalData[illData])
-            else:
-                sht1.write(rowBegin, 0, fqdn, bodyFont1)
-                sht1.write(rowBegin, 1, segNum, bodyFont2)
-                sht1.write(rowBegin, 2, '无', bodyFont2)
-                rowBegin += 1
+        if len(illegalData[illData]):
+            sht1.write_merge(rowBegin, rowBegin + len(illegalData[illData]) - 1, 0, 0, MdataDict['NFType'], bodyFont1)
+            sht1.write_merge(rowBegin, rowBegin + len(illegalData[illData]) - 1, 1, 1, MdataDict['region'], bodyFont2)
+            sht1.write_merge(rowBegin, rowBegin + len(illegalData[illData]) - 1, 2, 2, MdataDict['province'], bodyFont2)
+            sht1.write_merge(rowBegin, rowBegin + len(illegalData[illData]) - 1, 3, 3, nType, bodyFont2)
+            shtNum1 = rowBegin
+            for ld in illegalData[illData]:
+                sht1.write(shtNum1, 4, ld, bodyFont1)
+                shtNum1 = shtNum1 + 1
+            rowBegin += len(illegalData[illData])
+        else:
+            sht1.write(rowBegin, 0, MdataDict['NFType'], bodyFont1)
+            sht1.write(rowBegin, 1, MdataDict['region'], bodyFont2)
+            sht1.write(rowBegin, 2, MdataDict['province'], bodyFont2)
+            sht1.write(rowBegin, 3, nType, bodyFont2)
+            sht1.write(rowBegin, 4, '无', bodyFont1)
+            rowBegin += 1
 
     xls.save(XLSPath)
     logging.info('xls write end')
@@ -238,7 +334,7 @@ def main():
 
 
 if __name__ == '__main__':
-    # 750000~7AFFFF
-    print(int('750001', 16))
-    print(int('7AFFFF', 16))
+    # # 750000~7AFFFF
+    # print(int('750001', 16))
+    # print(int('7AFFFF', 16))
     main()
